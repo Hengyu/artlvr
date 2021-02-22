94ca220f2d306aecc75de203503657b1177c35c7
hengyu <hengyu@outlook.com>
2021-02-22 11:48:21 +0800

    Update catalyst release info

--- Sources/App/Data/ChessBotReleaseManager.swift
+++ Sources/App/Data/ChessBotReleaseManager.swift
@@ -14,24 +14,32 @@ public struct ChessBotReleaseManager {
 
     public enum Catalyst {
         public static var latestBeta: TypedRelease = TypedRelease.beta(
-            version: .init(3, 0, 0),
+            version: .init(3, 6, 0),
             platform: .init(platform: .macOS, version: .init(stringLiteral: "11.0"))
         )
 
         public static var latestProduction: TypedRelease = TypedRelease.production(
-            version: .init(2, 4, 0),
+            version: .init(3, 5, 0),
             platform: .init(platform: .macOS, version: .init(stringLiteral: "10.15"))
         )
 
-        /// Not submitted to App Store yet.
-        public static let v3_0: Release = .init(
-            version: .init(3, 0, 0),
+        /// Alpha
+        public static let v3_6: Release = .init(
+            version: .init(3, 6, 0),
             platform: .init(platform: .macOS, version: .init(stringLiteral: "11.0")),
             url: URL(string: "https://apps.apple.com/us/app/id1065334169")!,
             latestProduction: latestProduction
         )
 
-        /// Pending review.
+        /// In App Store. The latest version of ChessBot 3.x, for macOS 11.0+.
+        public static let v3_5: Release = .init(
+            version: .init(3, 5, 0),
+            platform: .init(platform: .macOS, version: .init(stringLiteral: "11.0")),
+            url: URL(string: "https://apps.apple.com/us/app/id1065334169")!,
+            latestProduction: latestProduction
+        )
+
+        /// In App Store. The latest version of ChessBot 2.x, for macOS 10.15+.
         public static let v2_4: Release = .init(
             version: .init(2, 4, 0),
             platform: .init(platform: .macOS, version: .init(stringLiteral: "10.15")),
@@ -39,7 +47,6 @@ public struct ChessBotReleaseManager {
             latestProduction: latestProduction
         )
 
-        /// In App Store.
         public static let v2_3: Release = .init(
             version: .init(2, 3, 0),
             platform: .init(platform: .macOS, version: .init(stringLiteral: "10.15")),
@@ -49,7 +56,8 @@ public struct ChessBotReleaseManager {
 
         public static func getRelease(_ version: Version) -> Release? {
             switch version.description {
-            case "3.0.0": return Catalyst.v3_0
+            case "3.6.0": return Catalyst.v3_6
+            case "3.5.0": return Catalyst.v3_5
             case "2.4.0": return Catalyst.v2_4
             case "2.3.0": return Catalyst.v2_3
             default: return nil
